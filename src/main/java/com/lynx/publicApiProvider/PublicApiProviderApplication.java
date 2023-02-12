06297ef40912dd3fcc5ff8a97a7b80206f507aac
denis <denislisaev@yandedx.ru>
2023-02-12 19:44:45 +0300

    Initial commit

---
+++ src/main/java/com/lynx/publicApiProvider/PublicApiProviderApplication.java
@@ -0,0 +1,13 @@
+package com.lynx.publicApiProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicApiProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicApiProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lynx/publicApiProvider/PublicApiProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynx.publicApiProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicApiProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
